0e7e84ac2180668d100f3b1a38915fb802445c38
MalikP1 <m.pinkerton7@outlook.com>
2024-08-01 15:18:51 +0100

    add colours, resets and images. Add company logo to the header.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,17 @@
 import "./Header.scss"
+import banner from "../../assets/images/cleaning-logo-banner.png";
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
-        <header>
-            This is the Header.
+        <header className="header">
+            <Link to="/" className="header__link">
+                <img 
+                    className="header__logo"
+                    src={banner}
+                    alt="E D and R cleaning company logo"
+                />
+            </Link>
         </header>
     )
 }
