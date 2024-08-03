967aa9d7dd7a356a39f80ffe076fe2791a8d5e3b
MalikP1 <m.pinkerton7@outlook.com>
2024-08-03 21:24:14 +0100

    complete footer with links to socials and email. Make website responsive for mobile, tablet and desktop.

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,22 @@
+import "./Footer.scss"
+import instagram from "../../assets/images/instagram-logo.svg";
+import facebook from "../../assets//images/facebook-logo.svg";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <section className="email">
+                <p className="email__text">Please contact us via email here: <a className="email__link" href="mailto:ello.beautysalon@gmail.com">E D&R Cleaning Company</a></p>
+            </section>
+            <section className="socials">
+                <p className="socials__text">Check out our socials!</p>
+                <article className="socials__article">
+                    <a className="socials__link" href="https://www.instagram.com/edandrcleaningcompany/"><img className="socials__image" src={instagram} alt="instagram logo"/></a>
+                    <a className="socials__link" href="https://www.facebook.com/edandrcleaningcompany/"><img className="socials__image" src={facebook} alt="facebook logo"/></a>
+                </article>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,7 @@ function Home() {
     return (
         <main className="home">
             <p className="home__text">Find the best cleaners in Leicester!</p>
-            <p className="home__text">Providing domestic and commerical cleaning available all hours of the day!</p>
+            <p className="home__text">Providing domestic and commercial cleaning available all hours of the day!</p>
             <p className="home__text">Please contact me on: <span className="home__text--bold">07366383277</span> for all enquiries.</p>
         </main>
     )
