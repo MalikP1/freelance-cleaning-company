3278fc84e108476a3f1819054a9ba2e794835675
MalikP1 <m.pinkerton7@outlook.com>
2024-08-04 23:45:02 +0100

    update company contact number.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
         <main className="home">
             <p className="home__text">Find the best cleaners in Leicester!</p>
             <p className="home__text">Providing domestic and commercial cleaning available all hours of the day!</p>
-            <p className="home__text">Please contact me on: <span className="home__text--bold">07366383277</span> for all enquiries.</p>
+            <p className="home__text">Please contact me on: <span className="home__text--bold">07300829619</span> for all enquiries.</p>
         </main>
     )
 }
