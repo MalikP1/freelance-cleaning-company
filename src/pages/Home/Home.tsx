748a7d57b26f6f352960dc240cf8a506a6f19810
MalikP1 <m.pinkerton7@outlook.com>
2024-08-14 14:07:17 +0100

    change text on home page.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,8 @@ function Home() {
     return (
         <main className="home">
             <p className="home__text">Find the best cleaners in Leicester!</p>
-            <p className="home__text">Providing domestic and commercial cleaning available all hours of the day!</p>
+            <p className="home__text">Providing domestic and commercial cleaning available all hours of the day.</p>
+            <p className="home__text">No job is too small!</p>
             <p className="home__text">Please contact me on: <span className="home__text--bold">07300829619</span> for all enquiries.</p>
         </main>
     )
