d62551cbe4a6b6a81cb8282e5e585fa9587df76c
MalikP1 <m.pinkerton7@outlook.com>
2024-09-09 09:15:07 +0100

    comment out imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import './App.scss';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Home from "./pages/Home/Home";
-import Footer from "./components/Footer/Footer";
+// import './App.scss';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Header from "./components/Header/Header";
+// import Home from "./pages/Home/Home";
+// import Footer from "./components/Footer/Footer";
 
 
 function App() {
